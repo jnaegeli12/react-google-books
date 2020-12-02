368b307533f07575984785880d74df1e9e9e7d4f
Judy Naegeli <jnaegeli@gmail.com>
2020-12-01 21:28:53 -0800

    deploying to heroku

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-  { 
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-  });
+  }
+);
 
 app.use("/api", routes);
 
